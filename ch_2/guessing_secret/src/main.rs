3942e5f5250a8e577ae671055fc0d8636a629aef
Revan.mac.M <45191163+PengfeiM@users.noreply.github.com>
2025-11-10 20:44:32 +0800

    feat: third party crate

---
+++ ch_2/guessing_secret/src/main.rs
@@ -0,0 +1,31 @@
+// based on former guessing number, we use a 'real' rand num for guessing.
+use std::io;
+
+use rand::Rng;
+
+// let's make a guessing game.
+fn main() {
+    println!("Guess the number!");
+
+    // immutable
+    // thread_rng: return a random number generator.
+    // gen_range: generate random number in range.
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    println!("Please input your guessses.");
+
+    // let is a keyword for create a variable.
+    // mtu makes the variable mutable, which means its value can be changed.
+    // without it, the variable will be immutable.
+    let mut guess = String::new();
+
+    // use stdin in io library
+    // an alternative way is 'std::io::stdin()' without use std::io.
+    io::stdin()
+        .read_line(&mut guess) // a standard func to read user's input, write into var guess.
+        .expect("Failed to read line"); // Fault handling func.
+
+    println!("You guessed: {guess}");
+}
