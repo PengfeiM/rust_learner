ec6d6e5c2a3dddaef7e309b77dae920a74c97011
Revan.wsl.M <45191163+PengfeiM@users.noreply.github.com>
2026-01-05 23:47:31 +0800

    feat: some fancy enum

--- ch_6/enums/src/enums.rs
+++ ch_6/enums/src/enums.rs
@@ -5,3 +5,16 @@ pub enum IpAddrVersion {
     V4,
     V6,
 }
+
+#[derive(Debug)]
+pub enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+impl Message {
+    pub fn print(&self) {
+        println!("here is a message: {:?}", self);
+    }
+}
--- ch_6/enums/src/main.rs
+++ ch_6/enums/src/main.rs
@@ -1,4 +1,4 @@
-use self::enums::IpAddrVersion;
+use self::enums::{IpAddrVersion, Message};
 
 mod enums;
 
@@ -6,6 +6,20 @@ mod enums;
 struct IpAddr {
     version: IpAddrVersion,
     address: String,
+}
+
+// Rather than use enum in a struct, we can put data directly into each enum varaint.
+#[derive(Debug)]
+enum IpAddressString {
+    V4(String),
+    V6(String),
+}
+
+// also, we can define it with numbers.
+#[derive(Debug)]
+enum IpAddressNum {
+    V4(u8, u8, u8, u8),
+    V6(u16, u16, u16, u16, u16, u16, u16, u16),
 }
 
 impl IpAddr {
@@ -29,4 +43,26 @@ fn main() {
         "Voila, we define deux ip address with enum: {:?} {:?}",
         home, loopback
     );
+
+    // IpAddressString
+    let home = IpAddressString::V4(String::from("127.0.0.1"));
+    let loopback = IpAddressString::V6(String::from("::1"));
+    println!(
+        "we define two ip address with enum: {:?} {:?}",
+        home, loopback
+    );
+
+    // IpAddressNum
+    let ip43 = IpAddressNum::V4(19, 58, 10, 09);
+    let ip63 = IpAddressNum::V6(0xfe80, 0, 0, 0, 0, 0, 0, 0x1);
+    println!(
+        "we define two ip address with enum containing number: {:?} {:?}",
+        ip43, ip63,
+    );
+
+    // try some fancy enum.
+    let m = Message::Write(String::from("hello"));
+    m.print();
+
+    // TODO: The Option Enum.
 }
